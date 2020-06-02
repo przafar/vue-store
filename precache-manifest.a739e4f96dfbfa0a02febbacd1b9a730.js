766ac8d58d0a7ee263015753a110bcc2a974a9c5
zafarpolvon <rasul.sadullaev@gmail.com>
2020-06-02 12:35:59 +0500

    deploy

---
+++ precache-manifest.a739e4f96dfbfa0a02febbacd1b9a730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4178c92d482d5d0862",
+    "url": "/vue-store/css/app.1a8ff1b2.css"
+  },
+  {
+    "revision": "fb021ac464729018a11db03b0341a80f",
+    "url": "/vue-store/index.html"
+  },
+  {
+    "revision": "de4178c92d482d5d0862",
+    "url": "/vue-store/js/app.c12daed2.js"
+  },
+  {
+    "revision": "e308bf6923e31cccd6a6",
+    "url": "/vue-store/js/chunk-vendors.72862eac.js"
+  },
+  {
+    "revision": "35eab73607a5c3efe4ddbc54ddf41ef1",
+    "url": "/vue-store/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-store/robots.txt"
+  }
+]);
