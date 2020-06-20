f7a95e1ac24383a7fff2e1e182f4bb7700ddc08d
zafarpolvon <rasul.sadullaev@gmail.com>
2020-06-20 06:16:14 +0500

    Review

---
+++ precache-manifest.cb0ec91216174945ee612d6609f3f260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02e52a57015b7c695ba",
+    "url": "/vue-store/css/app.0fbd79b2.css"
+  },
+  {
+    "revision": "001f4e8fb49ada61e77b3378bbc775bb",
+    "url": "/vue-store/index.html"
+  },
+  {
+    "revision": "f02e52a57015b7c695ba",
+    "url": "/vue-store/js/app.1fdb2daf.js"
+  },
+  {
+    "revision": "dd97f5128e31ab307832",
+    "url": "/vue-store/js/chunk-vendors.332336b2.js"
+  },
+  {
+    "revision": "35eab73607a5c3efe4ddbc54ddf41ef1",
+    "url": "/vue-store/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-store/robots.txt"
+  }
+]);
