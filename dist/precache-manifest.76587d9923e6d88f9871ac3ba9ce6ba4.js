9b1cc2f7a90802885d80a6f08d015364c44ba529
zafarpolvon <rasul.sadullaev@gmail.com>
2020-07-01 16:50:33 +0500

    Fixed Auth

---
+++ dist/precache-manifest.76587d9923e6d88f9871ac3ba9ce6ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87471cafa2b2da4fd3f1",
+    "url": "/vue-store/css/app.05ac63b5.css"
+  },
+  {
+    "revision": "c418145258d225dd1308e5aa2a70ab1d",
+    "url": "/vue-store/index.html"
+  },
+  {
+    "revision": "87471cafa2b2da4fd3f1",
+    "url": "/vue-store/js/app.d0024c17.js"
+  },
+  {
+    "revision": "bb612e7bdb7198dbd7cc",
+    "url": "/vue-store/js/chunk-vendors.5bdd0e38.js"
+  },
+  {
+    "revision": "35eab73607a5c3efe4ddbc54ddf41ef1",
+    "url": "/vue-store/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-store/robots.txt"
+  }
+]);
